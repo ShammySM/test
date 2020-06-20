adcd0f3a8d301b3c3d45fcbeade4a4e850423037
ShammySM <shammy.techarch@gmail.com>
2020-06-20 15:50:05 -0500

    added biryani

--- hungry.py
+++ hungry.py
@@ -1,2 +1,9 @@
-print('Hey buddy')
-print('new message')
+hungry=input('are you thirsty?')
+if hungry == 'yes':
+    print('eat samosa')
+    print('eat biryani')
+else:
+    thirsty=input('are you thirsty?')
+    if thirsty == 'yes':
+        print('drink water')
+        print('drink soda')

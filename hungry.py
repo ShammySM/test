3793949125290980357b8de019e36be13d6c835b
ShammySM <shammy.techarch@gmail.com>
2020-06-20 15:34:20 -0500

    added hungry and thirsty conditions

--- hungry.py
+++ hungry.py
@@ -1,2 +1,7 @@
-print('Hey buddy')
-print('new message')
+hungry=input('are you hungry?')
+if hungry == 'yes':
+    print('eat samosa')
+else:
+    thirsty=input('are you thirsty?')
+    if thirsty == 'yes':
+        print('drink water')

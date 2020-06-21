87e58d2dc8c470c95c716a96a56dc1e9cbcd2918
ShammySM <shammy.techarch@gmail.com>
2020-06-20 21:45:14 -0500

    new lines added

--- hungry.py
+++ hungry.py
@@ -1,9 +1,18 @@
 hungry=input('are you hungry?')
 if hungry == 'yes':
     print('eat samosa')
+    poor=input('are you poor?')
+    if poor=='yes':
+        print('sell 10 golden teapots')
+    else:
+        print('sell his money on robinhood')
 else:
     thirsty=input('are you thirsty?')
     if thirsty == 'yes':
         print('drink water')
         print('drink soda')
+    else:
+        print('do your home work')
+        print('sridhar get assembly square')
+
 
